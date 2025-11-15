431c68f4d92136b2c05dee1b6ff1eda1df9960ed
Matheus_Hslv <89474499+mhsilvaworks@users.noreply.github.com>
2025-11-14 23:14:12 -0300

    a
    a

---
+++ monorepo-pwa/apps/web/public/service-worker.js
@@ -0,0 +1,21 @@
+const CACHE = 'bootcamp-cache-v1';
+const ASSETS = [
+  '/',
+  '/index.html'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
+  );
+});
